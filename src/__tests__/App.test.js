6d83c0df5488ccb986a51f51b66429291f68d3c4
nazihadiyana <nazihadiyana@gmail.com>
2022-07-12 20:06:40 +0700

    frontend test

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { render } from "@testing-library/react";
+import { render, screen } from "@testing-library/react";
 import App from "../App";
 
 test("renders query dom", () => {
@@ -7,4 +7,14 @@ test("renders query dom", () => {
   const { container } = render(<App />);
   // `container` is just a DOM node
   container.querySelector("button");
+});
+
+test("render text", async () => {
+  render(<App />);
+
+  const linkElement = screen.getByText("React Axios GET");
+  expect(linkElement).toBeInTheDocument();
+
+  const linkElement1 = screen.getByText("React Axios POST");
+  expect(linkElement1).toBeInTheDocument();
 });
